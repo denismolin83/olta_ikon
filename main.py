ca0db25e038b6a6b0174e2aabc19f6a341e5e63f
1c_server <molind@inbox.ru>
2024-09-21 17:32:04 +0700

    three

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import openpyxl as op
-import os
 
-
-def xlsx_filename(folder_path):
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith('.xlsx'):  # если файл - xlsx
-            file_path = os.path.join(folder_path, file_name)  # получаем полный путь к файлу
-            return file_path  # возвращаем путь к файлу
-    else:
-        return None  # если xlsx файлы не найдены
-
+from utils.file_open import xlsx_filename
 
 file_olta_in = xlsx_filename("olta_in")
 file_sale_by_period = xlsx_filename("sale_by_period")
---
+++ utils/file_open.py
@@ -0,0 +1,11 @@
+import openpyxl as op
+import os
+
+
+def xlsx_filename(folder_path):
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.xlsx'):  # если файл - xlsx
+            file_path = os.path.join(folder_path, file_name)  # получаем полный путь к файлу
+            return file_path  # возвращаем путь к файлу
+    else:
+        return None  # если xlsx файлы не найдены
